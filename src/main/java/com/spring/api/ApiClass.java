bce023316e44c185d83a1e1e026ed00076725072
shvrekhan <shvrekhan@gmail.com>
2023-12-23 18:04:09 +0530

    1.0 => project setup.

---
+++ src/main/java/com/spring/api/ApiClass.java
@@ -0,0 +1,15 @@
+package com.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ApiClass.class, args);
+
+	}
+
+}
